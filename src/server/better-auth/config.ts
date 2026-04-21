836cf62bbf833365f825b6c4d6604845327df167
jbreemhaar <jsbreemhaar@gmail.com>
2026-04-21 09:01:08 +0200

    init commit

---
+++ src/server/better-auth/config.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+import { env } from "~/env";
+import { db } from "~/server/db";
+
+export const auth = betterAuth({
+  database: prismaAdapter(db, {
+    provider: "postgresql", // or "sqlite" or "mysql"
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
+
+export type Session = typeof auth.$Infer.Session;
